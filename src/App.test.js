74932d276a58e626c96525fbe08eec7265145553
Dmitry <dmitry27zh@gmail.com>
2024-03-27 19:50:12 +0500

    Use queryBy

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,20 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-test('renders learn react components', () => {
-  render(<App />)
-  const helloWorldElement = screen.getByText(/hello world/i)
-  const btn = screen.getByRole('button')
-  const input = screen.getByPlaceholderText(/input value.../i)
-  expect(helloWorldElement).toBeInTheDocument()
-  expect(btn).toBeInTheDocument()
-  expect(input).toMatchSnapshot()
-  screen.debug()
+describe('TEST_APP', () => {
+  test('renders learn react components', () => {
+    render(<App />)
+    const helloWorldElement = screen.getByText(/hello world/i)
+    const btn = screen.getByRole('button')
+    const input = screen.getByPlaceholderText(/input value.../i)
+    expect(helloWorldElement).toBeInTheDocument()
+    expect(btn).toBeInTheDocument()
+    expect(input).toMatchSnapshot()
+    screen.debug()
+  })
+  test('screen api', () => {
+    render(<App />)
+    const helloWorldElem = screen.queryByText(/hello2/i)
+    expect(helloWorldElem).toBeNull()
+  })
 })
