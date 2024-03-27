febfdcbd29e91b0331f9fcb95af3e9446c8a050b
Dmitry <dmitry27zh@gmail.com>
2024-03-27 19:45:00 +0500

    Test components

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders learn react components', () => {
+  render(<App />)
+  const helloWorldElement = screen.getByText(/hello world/i)
+  const btn = screen.getByRole('button')
+  const input = screen.getByPlaceholderText(/input value.../i)
+  expect(helloWorldElement).toBeInTheDocument()
+  expect(btn).toBeInTheDocument()
+  expect(input).toMatchSnapshot()
+  screen.debug()
+})
