a4b1487e90ba7577d1901b013bd15e2c7b6ef528
Dmitry <dmitry27zh@gmail.com>
2024-03-28 09:37:14 +0500

    Use findBy

--- src/App.test.js
+++ src/App.test.js
@@ -12,9 +12,14 @@ describe('TEST_APP', () => {
     expect(input).toMatchSnapshot()
     screen.debug()
   })
-  test('screen api', () => {
+  test('screen api', async () => {
     render(<App />)
     const helloWorldElem = screen.queryByText(/hello2/i)
     expect(helloWorldElem).toBeNull()
+    screen.debug()
+    const helloWorldElem2 = await screen.findByText(/data/i)
+    expect(helloWorldElem2).toBeInTheDocument()
+    screen.debug()
+    expect(helloWorldElem2).toHaveStyle({ color: 'red' })
   })
 })
