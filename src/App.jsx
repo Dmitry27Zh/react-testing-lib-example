2fb45800d95a074b9f76f746a382f19aaecaa738
Dmitry <dmitry27zh@gmail.com>
2024-03-28 09:55:42 +0500

    Test events

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from 'react'
 
 function App() {
   const [data, setData] = useState(null)
+  const [toggle, setToggle] = useState(false)
+  const [value, setValue] = useState('')
+  const onClick = () => setToggle((prev) => !prev)
   useEffect(() => {
     setTimeout(() => {
       setData({})
@@ -10,10 +13,14 @@ function App() {
 
   return (
     <div>
+      <h1 data-testid="value-elem">{value}</h1>
+      {toggle && <div data-testid="toggle-elem">toggle</div>}
       {data && <div style={{ color: 'red' }}>data</div>}
-      <h1>Hello World!</h1>
-      <button>Click me</button>
-      <input type="text" placeholder="input value..." />
+      <h2>Hello world</h2>
+      <button data-testid="toggle-btn" onClick={onClick}>
+        Click me
+      </button>
+      <input type="text" placeholder="input value..." onChange={(e) => setValue(e.target.value)} />
     </div>
   )
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import App from './App'
 
 describe('TEST_APP', () => {
@@ -21,5 +21,25 @@ describe('TEST_APP', () => {
     expect(helloWorldElem2).toBeInTheDocument()
     screen.debug()
     expect(helloWorldElem2).toHaveStyle({ color: 'red' })
+  })
+  test('click event', () => {
+    render(<App />)
+    const btn = screen.getByTestId('toggle-btn')
+    expect(screen.queryByTestId('toggle-elem')).toBeNull()
+    fireEvent.click(btn)
+    expect(screen.queryByTestId('toggle-elem')).toBeInTheDocument()
+    fireEvent.click(btn)
+    expect(screen.queryByTestId('toggle-elem')).toBeNull()
+  })
+  test('input event', () => {
+    render(<App />)
+    const input = screen.getByPlaceholderText(/input value.../i)
+    const valueElem = screen.queryByTestId('value-elem')
+    const testValue = 'afafsfs'
+    expect(valueElem).toContainHTML('')
+    fireEvent.input(input, {
+      target: { value: testValue },
+    })
+    expect(valueElem).toContainHTML(testValue)
   })
 })
